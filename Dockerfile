f497b6a823e73991892408c7234097251e249c8d
Michael Dockter <michael@dockter.com>
2019-10-11 22:49:39 -0400

    Original

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM centos:7
+
+# Copy files from repository.
+
+COPY ./artifacts /inside
+COPY ./speed-test.py .
+
+# Runtime execution.
+
+CMD ["speed-test.py"]
---
+++ speed-test.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import time
+
+file_names = [
+    "/inside/big-file.txt",
+    "/outside/big-file.txt",
+]
+
+
+for file_name in file_names:
+    start_time = time.time()
+
+    byte_count = 0
+    with open(file_name, "rb") as file:
+        byte = file.read(1)
+        while byte != "":
+            byte_count += 1
+            byte = file.read(1)
+
+    elapsed_time = time.time() - start_time
+    byte_rate = byte_count / elapsed_time
+    print("elapsed time: {0}  bps: {1}  File: {2}".format(elapsed_time, byte_rate, file))
